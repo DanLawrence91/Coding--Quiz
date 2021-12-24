0c0f53b0681375e23fd8fd91082588bd37fb2252
Daniel Lawrence <dan.lawrence0810@gmail.com>
2021-12-24 18:24:33 +1030

    HTML landing page started

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+/* 
+1 - highscores page linked to main page - this is the time html changes
+2 - Landing page has title followed by some text describing the game
+3 - Start button on page will then hide this text and show first question
+4 - timer in top corner that begins when start is pressed
+5 - once first question is answered this then shows whether correct or incorrect for brief period
+6 - will then move to next question and display this with first question hidden
+7 - This will repeat for however many questions written
+8 - if question is answered incorrectly the timer will reduce by 10 seconds
+9 - once timer is over (clear interval) or all questions are answered the game will end
+10 - once time is up will display score and user will input initials
+11 - once initials are entered will be taken to highscore.html
+12 - this will display all scores save through local storage
+13 - on highscore page you can either go back to main page to start again or clear highscores
+14 - if highscores cleared the leaderboard disappears and you can then click button to go back
+15 - main page once quiz starts will have highscores link in top corner, timer in other corner and then question
+16 - html will not change as questions are answered and instead will change from display hidden to display visible
+17 - highscores button disabled whilst quiz in progress
+*/
