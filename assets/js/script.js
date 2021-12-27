50585999becc292caa8804f2a6b05de9893843d5
Daniel Lawrence <dan.lawrence0810@gmail.com>
2021-12-27 20:42:06 +1030

    html added to file with answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,20 @@
-/* 
-1 - highscores page linked to main page - this is the time html changes
-2 - Landing page has title followed by some text describing the game
-3 - Start button on page will then hide this text and show first question
-4 - timer in top corner that begins when start is pressed
-5 - once first question is answered this then shows whether correct or incorrect for brief period
-6 - will then move to next question and display this with first question hidden
-7 - This will repeat for however many questions written
-8 - if question is answered incorrectly the timer will reduce by 10 seconds
-9 - once timer is over (clear interval) or all questions are answered the game will end
-10 - once time is up will display score and user will input initials
-11 - once initials are entered will be taken to highscore.html
-12 - this will display all scores save through local storage
-13 - on highscore page you can either go back to main page to start again or clear highscores
-14 - if highscores cleared the leaderboard disappears and you can then click button to go back
-15 - main page once quiz starts will have highscores link in top corner, timer in other corner and then question
-16 - html will not change as questions are answered and instead will change from display hidden to display visible
-17 - highscores button disabled whilst quiz in progress
-*/
+// - Landing page has title followed by some text describing the game - DONE
+// - Start button on page will then hide this text and show first question
+// - questions and answers are hidden until start is pressed
+// - questions will only appear one at a time and next question won't appear until previous one answered
+// - timer in top corner that begins when start is pressed (set interval)
+// - need to then show question with four answer buttons for user to select one
+// - once first question is answered this then shows whether correct or incorrect for brief period (use interval)
+// - will then move to next question and display this with first question hidden
+// - This will repeat for however many questions written
+// - if question is answered incorrectly the timer will reduce by 10 seconds
+// - once timer is over (clear interval) or all questions are answered the game will end
+// - once time is up will display score and user will input initials using form and submit button
+// - highscores page linked to main page - this is the time html changes
+// - once initials are entered will be taken to highscore.html - look at score apps from class in week 4
+// - this will display all scores save through local storage
+// - on highscore page you can either go back to main page to start again or clear highscores
+// - if highscores cleared the leaderboard disappears and you can then click button to go back
+// - main page once quiz starts will have highscores link in top corner, timer in other corner and then question
+// - html will not change as questions are answered and instead will change from display hidden to display visible
+// - highscores button disabled whilst quiz in progress
