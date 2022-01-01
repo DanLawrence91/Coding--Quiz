0c05bfe96db68dc959c4c667e9db0adec41eef7b
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-01-02 10:11:38 +1030

    highscores now show in order

--- HSscript.js
+++ HSscript.js
@@ -5,10 +5,25 @@ const leaderboardEl = document.querySelector("#high-score")
 
 const hS = JSON.parse(localStorage.getItem("hS")) || [];
 
+function renderHS(){
+    leaderboardEl.innerHTML = "";
+
+    for (var i = 0; i < hS.length; i++){
+        var highS = hS[i];
+
+        var li = document.createElement("li");
+        li.textContent = highS.initials + " - " + highS.score
+
+        leaderboardEl.appendChild(li)
+    }
+}
+
+renderHS ()
+
 //adds scores stored in local storage to the highscores leaderboard
-leaderboardEl.innerHTML = hS.map(highscore => {
-    return `<li class="score-list">${highscore.initials}: ${highscore.score}</li>`
-}).join("")
+// leaderboardEl.innerHTML = hS.map(highscore => {
+//     return `<li class="score-list">${highscore.initials}: ${highscore.score}</li>`
+// }).join("")
 
 // return button on highscore page
 returnBtnEl.addEventListener("click", function(){
